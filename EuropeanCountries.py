4fb508de5fad0aa611592f9d4695b1e82b1687d8
R <r.aguirre62@gmail.com>
2017-03-13 16:01:34 -0700

    This code compiltes Twitter's European trends.

---
+++ EuropeanCountries.py
@@ -0,0 +1,202 @@
+#This code is compiles the Twitter trends in European. Below are countries
+#That are not utilizing Twitter's API
+
+import twitter
+import json
+
+AUSTRIA_WOE_ID = 23424750
+austria_trends = twitter_api.trends.place(_id=AUSTRIA_WOE_ID)
+
+print(json.dumps(austria_trends, indent=1)
+      
+austria_trends_set = set([trend['name']
+                        for trend in austria_trends[0]['trends']])
+
+
+BELGIUM_WOE_ID = 23424757
+
+belgium_trends = twitter_api.trends.place(_id=BELGIUM_WOE_ID)
+
+print(json.dumps(belgium_trends, indent=1)
+      
+belgium_trends_set = set([trend['name']
+                        for trend in belgium_trends[0]['trends']])
+
+
+DENMARK_WOE_ID = 23424796
+
+denmark_trends = twitter_api.trends.place(_id=DENMARK_WOE_ID)
+
+print(json.dumps(austria_trends, indent=1)
+      
+denmark_trends_set = set([trend['name']
+                        for trend in denmark_trends[0]['trends']])
+
+
+FRANCE_WOE_ID = 23424819
+
+france_trends = twitter_api.trends.place(_id=FRANCE_WOE_ID)
+
+print(json.dumps(austria_trends, indent=1)
+      
+france_trends_set = set([trend['name']
+                        for trend in france_trends[0]['trends']])
+
+
+GERMANY_WOE_ID = 23424829
+
+germany_trends = twitter_api.trends.place(_id=GERMANY_WOE_ID)
+
+print(json.dumps(germany_trends, indent=1)
+      
+germany_trends_set = set([trend['name']
+                        for trend in germany_trends[0]['trends']])
+
+
+GREECE_WOE_ID = 23424833
+
+greece_trends = twitter_api.trends.place(_id=GREECE_WOE_ID)
+
+print(json.dumps(greece_trends, indent=1)
+      
+greece_trends_set = set([trend['name']
+                        for trend in greece_trends[0]['trends']])
+
+
+IRELAND_WOE_ID = 23424803
+      
+ireland_trends = twitter_api.trends.place(_id=IRELAND_WOE_ID)
+
+print(json.dumps(ireland_trends, indent=1)
+      
+ireland_trends_set = set([trend['name']
+                        for trend in ireland_trends[0]['trends']])
+
+
+ITALY_WOE_ID = 23424853
+
+italy_trends = twitter_api.trends.place(_id=ITALY_WOE_ID)
+
+print(json.dumps(italy_trends, indent=1)
+      
+italy_trends_set = set([trend['name']
+                        for trend in italy_trends[0]['trends']])
+
+
+LATVIA_WOE_ID = 23424874
+
+latvia_trends = twitter_api.trends.place(_id=LATVIA_WOE_ID)
+
+print(json.dumps(latvia_trends, indent=1)
+      
+latvia_trends_set = set([trend['name']
+                        for trend in latvia_trends[0]['trends']])
+
+
+NETHERLANDS_WOE_ID = 23424909
+
+netherlands_trends = twitter_api.trends.place(_id=NETHERLANDS_WOE_ID)
+
+print(json.dumps(netherlands_trends, indent=1)
+      
+netherlands_trends_set = set([trend['name']
+                        for trend in netherlands_trends[0]['trends']])
+
+  
+NORWAY_WOE_ID = 23424910
+
+norway_trends = twitter_api.trends.place(_id=NORWAY_WOE_ID)
+
+print(json.dumps(norway_trends, indent=1)
+      
+norway_trends_set = set([trend['name']
+                        for trend in norway_trends[0]['trends']])
+
+
+POLAND_WOE_ID = 23424923
+
+poland_trends = twitter_api.trends.place(_id=POLAND_WOE_ID)
+
+print(json.dumps(poland_trends, indent=1)
+      
+poland_trends_set = set([trend['name']
+                        for trend in poland_trends[0]['trends']])
+
+
+PORTUGAL_WOE_ID = 23424925
+
+portugal_trends = twitter_api.trends.place(_id=PORTUGAL_WOE_ID)
+
+print(json.dumps(portugal_trends, indent=1)
+      
+portugal_trends_set = set([trend['name']
+                        for trend in portugal_trends[0]['trends']])
+
+RUSSIA_WOE_ID = 23424936
+
+russia_trends = twitter_api.trends.place(_id=RUSSIA_WOE_ID)
+
+print(json.dumps(russia_trends, indent=1)
+      
+russia_trends_set = set([trend['name']
+                        for trend in russia_trends[0]['trends']])
+
+
+SPAIN_WOE_ID = 23424950
+
+spain_trends = twitter_api.trends.place(_id=SPAIN_WOE_ID)
+
+print(json.dumps(spain_trends, indent=1)
+      
+spain_trends_set = set([trend['name']
+                        for trend in spain_trends[0]['trends']])
+
+
+SWEDEN_WOE_ID = 23424954
+
+sweden_trends = twitter_api.trends.place(_id=SWEDEN_WOE_ID)
+
+print(json.dumps(sweden_trends, indent=1)
+      
+sweden_trends_set = set([trend['name']
+                        for trend in sweden_trends[0]['trends']])
+
+
+SWITZERLAND_WOE_ID = 23424957
+
+switzerland_trends = twitter_api.trends.place(_id=SWITZERLAND_WOE_ID)
+
+print(json.dumps(switzerland_trends, indent=1)
+      
+switzerland_trends_set = set([trend['name']
+                        for trend in switzerland_trends[0]['trends']])
+
+
+TURKEY_WOE_ID = 23424969
+
+turkey_trends = twitter_api.trends.place(_id=TURKEY_WOE_ID)
+
+print(json.dumps(turkey_trends, indent=1)
+      
+turkey_trends_set = set([trend['name']
+                        for trend in turkey_trends[0]['trends']])
+
+
+UKRAINE_WOE_ID = 23424976
+
+ukraine_trends = twitter_api.trends.place(_id=UKRAINE_WOE_ID)
+
+print(json.dumps(ukraine_trends, indent=1)
+      
+ukraine_trends_set = set([trend['name']
+                        for trend in ukraine_trends[0]['trends']])
+
+
+UK_WOE_ID = 23424975
+
+uk_trends = twitter_api.trends.place(_id=UK_WOE_ID)
+
+print(json.dumps(uk_trends, indent=1)
+      
+uk_trends_set = set([trend['name']
+                        for trend in uk_trends[0]['trends']])
